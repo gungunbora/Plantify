fd9e8fb2e3a8223124bc5ef2412dca3da9b543cc
gungun <boragungun7@gmail.com>
2026-01-01 23:12:57 +0530

    new commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,30 @@
+import mainimage from  "../assets/images/plants.png";
+
+const HeroSection =()=>{
+    return(
+        <section className="bg-[#eef6ec] min-h-[85vh] flex items-center">
+             <div className="max-w-[1300px] mx-auto w-full flex items-center justify-between px-12">
+            {/*LEFT */}
+     <div className="max-w-2xl">
+     <h1 className=" font-playfair text-[84px] font-bold text-[#3f6b4f] leading-[1.1]">
+        HOME IS <br />
+        WHERE <br />
+        THE PLANTS <br />
+        ARE
+     </h1> 
+     <p className="mt-10 text-[#4b5f52] text-base max-w-lg">
+       Bring nature into your home with beautiful and healthy plants.
+     </p>
+     </div>
+
+     {/*RIGHT */}
+     <div className=" flex justify-end w-[45%]">
+        <img src={mainimage} alt="mainimage" 
+        className="w-[620px] transition-all duration-500 ease-out hover:scale-105 hover:-translate-y-2 
+         animate-[float_8s_ease-in-out_infinite]" /> 
+     </div>
+     </div>
+     </section>
+    );
+};
+export default HeroSection;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import {Link} from "react-router-dom";
+import logo from "../assets/images/leaficon.ico"; 
+
+const Navbar = () =>{
+  return(
+    <nav className="w-full bg-green-50 px-10 py-4 flex items-center justify-between"> 
+
+    {/* LEFT */}
+    <div className="flex items-center gap-2">
+      <img src={logo} alt="Plantify logo" className="w-7 h-7"/>
+      <span className="text-green-700 font-bold text-xl">
+        PLANTIFY
+      </span>
+    </div>
+
+    {/* RIGHT */}
+    <ul className="flex items-center gap-6 text-sm font-medium">
+      <li>
+        <Link to="/home"className="hover:text-green-700">
+        Home</Link>
+      </li>
+      <li>
+        <Link to="/shop"className="hover:text-green-700">
+        Shop</Link>
+      </li>
+      <li>
+        <Link to="about"className="hover:text-green-700">
+        About Plants </Link>
+      </li>
+      <li><Link to="/login" className="hover:text-green-700">
+       Login</Link>
+      </li>
+      <li><Link to="/signup" className="hover:text-green-700">
+      Sign Up</Link>
+      </li>
+
+    </ul>
+    </nav>
+
+  );
+};
+export default Navbar;
